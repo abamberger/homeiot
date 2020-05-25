75753163883e1105ff584b7662e1cfadb5e6ba51
Alex <alexander.bamberger@gmail.com>
2020-05-25 01:46:46 +0100

    first commit

---
+++ HomeIoTdb.py
@@ -0,0 +1,105 @@
+import psycopg2
+import datetime
+
+class HomeIoT(object):
+    def __init__(self, conn):
+        """
+        Supply a connection string for connecting to the database. Must be a dictionary. Requires:
+            host, database, user & password
+        """
+        self._validate_conn(conn)
+        self.conn = conn
+
+    def create_new_sensor(self, name, units = ''):
+        """
+        Creates a new sensor entry in the database. name must not be present in the database.
+        name - the varchar name. Required
+        units - the units in varchar. Not required.
+        """
+        con = psycopg2.connect(**self.conn)
+        cur = con.cursor()
+
+        q = "SELECT * FROM sensors WHERE name = '{0}'".format(name)
+        cur.execute(q)
+        names = cur.fetchall()
+
+        if len(names) > 0:
+            print('Note: Sensor {0} already exists in the database.')
+        else:
+            q = "INSERT INTO sensors (name, units) VALUES ('{0}', '{1}')".format(name, units)
+            cur.execute(q)
+            con.commit()
+
+        con.close()
+
+    def insert_data(self, sensorname, value, timestamp = None):
+        """
+        Inserts a data point. provide the varchar name of the sensor and the value.
+        Timestamp is optional. If not Provided the current system time will be used.
+        """
+        con = psycopg2.connect(**self.conn)
+        cur = con.cursor()
+
+        q = "SELECT id FROM sensors WHERE name = '{0}'".format(sensorname)
+        cur.execute(q)
+        ids = cur.fetchall()
+
+        if len(ids) == 0:
+            raise Exception('Sensor {0} does not exist in the database. Add it first using method create_new_sensor'.format(sensorname))
+
+        ids = ids[0][0]
+
+        if timestamp == None:
+            q = "INSERT INTO sensordata (sensorid, datatime, value) VALUES ({0}, now(), {1})".format(ids, value)
+        else:
+            q = "INSERT INTO sensordata (sensorid, datatime, value) VALUES ({0}, {1}, {2})".format(ids, datetime.datetime.now, value)
+
+        cur.execute(q)
+        con.commit()
+
+        con.close()
+
+    def _validate_conn(self, conn):
+        req = ['host', 'database', 'user', 'password']
+
+        if type(conn) != dict:
+            raise Exception('conn string is not a dictionary. Must be a dictionary')
+
+        for r in req:
+            if r not in conn:
+                raise Exception('{0} not in supplied database connection string'.format(r))
+
+
+if __name__ == "__main__":
+    conn = {'host': '192.168.1.251'
+            , 'database': 'home'
+            , 'user': 'homeiot'
+            , 'password': 'iotp@ss'}
+    ht = HomeIoT(conn)
+
+    ht.create_new_sensor(name = 'temp_study')
+
+
+    ht.insert_data('temp_study', 44.)
+    ht.insert_data('temp_study', 56.)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
